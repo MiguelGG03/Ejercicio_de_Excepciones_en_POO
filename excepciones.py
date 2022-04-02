8842188fc16dd8b48319b19d98a419616ccedfe3
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-02 18:47:41 +0200

    par de cambios

--- excepciones.py
+++ excepciones.py
@@ -3,9 +3,9 @@ import re
 
 correo = input(str('Introduzca su direccion de correo electronico:\n'))
 try:
-    if(re.search("* @*.*", correo)!=None):
+    if(re.search(".*@.*..*", correo)!=None):
         print('Acceso completado')
     else:
         print(1/0)
 except ZeroDivisionError:
-    print('**ERROR**\nEso no es una direccion de correo electronico valida')
+    print('**ERROR**\n'+str(correo)+'no es una direccion de correo electronico valida')
