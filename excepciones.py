eb014705589aa15931ec4d2e75fc66f2373a9503
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-02 18:51:18 +0200

    algoritmo para expulsar despues de 3 intentos

--- excepciones.py
+++ excepciones.py
@@ -1,11 +1,20 @@
 import re
 
 
-correo = input(str('Introduzca su direccion de correo electronico:\n'))
-try:
-    if(re.search(".*@.*..*", correo)!=None):
-        print('Acceso completado')
+acabar=False
+i=0
+while(acabar==False):
+    if(i<3):    
+        correo = input(str('Introduzca su direccion de correo electronico:\n'))
+        try:
+            if(re.search(".*@.*..*", correo)!=None):
+                print('Acceso completado')
+                acabar=True
+            else:
+                print(1/0)
+        except ZeroDivisionError:
+            print('**ERROR**\n'+str(correo)+'no es una direccion de correo electronico valida')
     else:
-        print(1/0)
-except ZeroDivisionError:
-    print('**ERROR**\n'+str(correo)+'no es una direccion de correo electronico valida')
+        print('**ERROR**\nCuenta bloqueada a causa de un ataque')
+        acabar=True
+    i=i+1
