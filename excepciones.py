816af0e4231f65324022d0c6cde689cce5449f89
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-04-02 18:42:40 +0200

    try añadido

--- excepciones.py
+++ excepciones.py
@@ -1,1 +1,9 @@
 import re
+
+
+correo = input(str('Introduzca su direccion de correo electronico:\n'))
+try:
+    if(re.search("* @*.*", correo)!=None):
+        print('Acceso completado')
+    else:
+        print(1/0)
